6b2d19e2f8e4a7d680987809ccfc147693d0b591
EstanislauFilho <estanislau.sena@gmail.com>
2019-01-04 16:52:05 -0200

    Adicionado variáveis para quantidade fotos a ser capturadas e conexão com webcam interna estabelecida.

--- faces_capture.py
+++ faces_capture.py
@@ -10,3 +10,11 @@ face_detect = cv2.CascadeClassifier("Classificador/haarcascade_frontalface_defau
 eye_detect = cv2.CascadeClassifier("Classificador/haarcascade-eye.xml")
 
 video = cv2.VideoCapture(0)
+
+cont_foto = 1
+total_photos = 50
+
+print("Capturing photos...")
+
+while(True):
+    conect, image_video = video.read()
