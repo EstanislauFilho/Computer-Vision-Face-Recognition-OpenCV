e7486875f3ff48e6b56d1980c825c41549d8afcc
EstanislauFilho <estanislau.sena@gmail.com>
2019-01-17 15:29:23 -0200

    Criação dos vetores que receberão as imagens para treinamento.

--- faces_training.py
+++ faces_training.py
@@ -4,3 +4,13 @@ Projeto: Reconhecimento Facial
 '''
 
 
+import cv2
+import os
+import numpy as np
+
+eigenface = cv2.face.EigenFaceRecognizer_create()
+fisherface = cv2.face.FisherFaceRecognizer_create()
+lbph = cv2.face.LBPHFaceRecognizer_create()
+
+def getImage():
+
