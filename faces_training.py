63253b8813e216cd90fc902903534b90ddcdd7e1
EstanislauFilho <estanislau.sena@gmail.com>
2019-01-17 16:39:53 -0200

    Criado funções para fazer o treinamento das imagens das faces.

--- faces_training.py
+++ faces_training.py
@@ -8,7 +8,7 @@ import cv2
 import os
 import numpy as np
 
-eingenface = cv2.face.EigenFaceRecognizer_create(num_components=50)
+eigenface = cv2.face.EigenFaceRecognizer_create(num_components=50)
 fisherface = cv2.face.FisherFaceRecognizer_create(num_components=50)
 lbph = cv2.face.LBPHFaceRecognizer_create()
 print("ok")
@@ -29,3 +29,16 @@ def getImage():
 
 
 ids, faces = getImage()
+
+print("Iniciando Treinamento...")
+
+eigenface.train(faces, ids)
+eigenface.write('Classificador/haarcascade_eigenface_face_recognition.yml')
+
+fisherface.train(faces, ids)
+fisherface.write('Classificador/haarcascade_fisherface_face_recognition.yml')
+
+lbph.train(faces, ids)
+lbph.write('Classificador/haarcascade_lbph_face_recognition.yml')
+
+print("Treinamento realizado com sucesso!")
