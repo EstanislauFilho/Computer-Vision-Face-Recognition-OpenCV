39e1b124f8a9252fcb13ea17c6708de646cad076
EstanislauFilho <estanislau.sena@gmail.com>
2019-01-17 15:52:13 -0200

    Leitura da pasta com as imagens das faces para treinamento.

--- faces_training.py
+++ faces_training.py
@@ -8,9 +8,16 @@ import cv2
 import os
 import numpy as np
 
-eigenface = cv2.face.EigenFaceRecognizer_create()
-fisherface = cv2.face.FisherFaceRecognizer_create()
+eingenface = cv2.face.EigenFaceRecognizer_create(num_components=50)
+fisherface = cv2.face.FisherFaceRecognizer_create(num_components=50)
 lbph = cv2.face.LBPHFaceRecognizer_create()
+print("ok")
+
 
 def getImage():
+    faces_path = [os.path.join('Faces_capturadas/',f) for f in os.listdir('Faces_capturadas/')]
+    faces = []
+    ids = []
+
+    print("ok")
 
