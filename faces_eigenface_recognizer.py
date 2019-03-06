5bf3410a27fb905ecd3e14a82909aa25ad322c0a
EstanislauFilho <estanislau.sena@gmail.com>
2019-03-06 20:05:16 -0300

    Criado variáveis com dimensões máxima de cada imagem, variável para chamada da função VideoCapture, e por fim, outra variável para tamanho da fonte.

--- faces_eigenface_recognizer.py
+++ faces_eigenface_recognizer.py
@@ -9,3 +9,9 @@ face_detect = cv2.CascadeClassifier("Classificador/haarcascade_frontalface_defau
 
 recognizer = cv2.face.EigenFaceRecognizer_create()
 recognizer.read("nome.yml")
+
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+width_face, heigth_face = 220, 220
+
+video = cv2.VideoCapture(0)
