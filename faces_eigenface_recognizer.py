84328251cf7c6fe1232e4928053df74401e37831
EstanislauFilho <estanislau.sena@gmail.com>
2019-03-06 20:19:02 -0300

    Criado variável name, para definir nome do usuário a ser reconhecido, e criado laço de repetição.

--- faces_eigenface_recognizer.py
+++ faces_eigenface_recognizer.py
@@ -8,10 +8,15 @@ import cv2
 face_detect = cv2.CascadeClassifier("Classificador/haarcascade_frontalface_default.xml")
 
 recognizer = cv2.face.EigenFaceRecognizer_create()
-recognizer.read("nome.yml")
+#recognizer.read("Classificador/nome.yml")
 
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
 width_face, heigth_face = 220, 220
 
 video = cv2.VideoCapture(0)
+
+name = ''
+
+while(True):
+
