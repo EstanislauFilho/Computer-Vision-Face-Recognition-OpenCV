87941aee194df2242b5a0ccc1ef3543962857632
EstanislauFilho <estanislau.sena@gmail.com>
2019-03-09 09:02:54 -0300

    Criado variável para fazer o redimensionamento das imagens das faces em escala de cinza, o que otimiza o algoritmo no reconhecimento. Projeção de um retangulo vermelho na face detectada pela câmera.

--- faces_lbph_recognizer.py
+++ faces_lbph_recognizer.py
@@ -24,3 +24,7 @@ while(True):
     image_video_gray = cv2.cvtColor(image_video, cv2.COLOR_BGR2GRAY)
 
     faces_on_video = face_detect.detectMultiScale(image_video_gray, scaleFactor=1.5, minSize=(100, 100))
+
+    for (x, y, width, heigth) in faces_on_video:
+        image_Face = cv2.resize(image_video_gray[y:y + heigth, x:x + width], (width_face, heigth_face))
+        cv2.rectangle(image_video, (x, y), (x + width, y + heigth), (0, 255, 0), 2) 
