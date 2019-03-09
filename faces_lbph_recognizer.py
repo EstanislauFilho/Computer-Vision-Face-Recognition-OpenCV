827438c253c92e64fd0c7daf88f8201b65725985
EstanislauFilho <estanislau.sena@gmail.com>
2019-03-09 09:01:35 -0300

    Criado variável responsável por fazer a detecção facial a partir da imagem em escala de cinza e de acordo com os parâmetros determinados.

--- faces_lbph_recognizer.py
+++ faces_lbph_recognizer.py
@@ -21,4 +21,6 @@ name = ''
 while(True):
     conect, image_video = video.read()
 
-    image_video_gray = cv2.cvtColor(image_video, cv2.COLOR_BGR2GRAY) 
+    image_video_gray = cv2.cvtColor(image_video, cv2.COLOR_BGR2GRAY)
+
+    faces_on_video = face_detect.detectMultiScale(image_video_gray, scaleFactor=1.5, minSize=(100, 100))
